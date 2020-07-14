7d99359f2c245e98ca8d6c0203b14ab4180bc6dd
Julia Shybunka <julishybunka@gmail.com>
2020-07-14 10:54:02 +0300

    Database Encryption - mongoose-encryption

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+var encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -15,6 +16,9 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
+
+const secret = 'Thisisyourlittlesecret.';
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 
 const User = mongoose.model("User", userSchema)
 
